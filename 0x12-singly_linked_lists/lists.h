0ab0413f2be22291eb498110833fc482c2edbf32
Meertechnology <78769992+Meertechnology@users.noreply.github.com>
2024-08-21 16:44:59 +0100

    updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,7 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTSH
+#define LISTSH
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
 
 /**
  * struct list_s - singly linked list
@@ -13,19 +10,21 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
-typedef struct	list_s
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
 
-size_t	print_list(const list_t *h);
-void	first(void) __attribute__ ((constructor));
-size_t	list_len(const list_t *h);
-list_t	*add_node_end(list_t **head, const char *str);
-list_t	*add_node(list_t **head, const char *string);
-void	free_list(list_t *head);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+int _putchar(char c);
 
 #endif
